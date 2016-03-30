46ee75fec35fc33deec87f4cdc38ddf904e4f19b
jeffmiralles <sponge2126bbob@yahoo.com>
2016-03-31 02:52:29 +0800

    refactor(moved-file): moved logger.js file into main file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
 'use strict';
+
+var clientConnector = require( './lib/clientConnector.js' );
+var severities = [ 'error', 'warn', 'info', 'verbose', 'debug', 'silly' ];
+var dateFormat = require( 'dateformat' );
+var socket = clientConnector.socket;
+var pscope;
+var message;
+
+var Logger = function( name, addr ) {
+  clientConnector.startConnecting( addr );
+  return {
+    level: severities[ 2 ],
+    time: function() {
+      return new Date();
+    },
+
+    pscope: 'server',
+    store: name,
+    info: function( message ) {
+      this.level = severities[ 2 ];
+      this.log( 'info', message, function() {
+
+      } );
+
+      return this;
+    },
+
+    scope: function( msg ) {
+      this.pscope = msg;
+      return this;
+    },
+
+    log: function( sevLevel, options, callback ) {
+      if ( options === undefined ) {
+        callback( 'sevLevel and message must not be undefined.' );
+        return;
+      }
+
+      if ( typeof options !== 'string' ) {
+        this.pscope = options.scope !== undefined ? options.scope : 'server';
+        message = options.pdata;
+      } else {
+        pscope = ( options.scope !== undefined ) ? options.scope : 'server';
+        message = options;
+      }
+
+      var sendLog = {
+        ref: 4501,
+        payload: {
+          operation: 'send',
+          store: this.store,
+          pdata: message,
+          pscope: this.pscope,
+          level: sevLevel,
+          timestamp:dateFormat( 'mmmm dd HH:MM:ss' )
+        }
+      };
+      socket.send( JSON.stringify( sendLog ) );
+      console.log( JSON.stringify( sendLog ) );
+      callback( null );
+      this.pscope = 'server'; //Resets the property scope to server
+      //just return a resolved promise for sending a log to socket
+      return Promise.resolve( 'sending of logs to server success' );
+    },
+
+    socket: socket
+  };
+};
+
+module.exports = Logger;
