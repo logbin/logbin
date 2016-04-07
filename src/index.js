9ebf05ef8437a8f29359ffc6a35359edc9f376bf
jeffmiralles <sponge2126bbob@yahoo.com>
2016-04-08 02:24:56 +0800

    feat(logger-opts): added password property as an option

--- src/index.js
+++ src/index.js
@@ -22,7 +22,9 @@ inbound.on( 'message', ( response ) => {
 var Logger = function( opts ) {
 
   // jscs: disable
-  inbound.plain_password = 'EkjFpCW0x';
+  if ( opts.password ) {
+     inbound.plain_password = 'EkjFpCW0x';
+  }
   // jscs: enable
   inbound.connect( opts.uri || 'tcp://127.0.0.1:5555' );
   this.store = opts.store;
