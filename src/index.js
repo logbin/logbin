3b2f3117b306cddb8ba3418661f7c28a72837931
Christopher Clint B. Pacillos <clint.pacillos@gmail.com>
2016-04-07 23:56:44 +0800

    tests(logger-api): add tests for returned promises * dev issue: https://identifi.atlassian.net/browse/LS-92 * rework logger api object * use co-mocha to test for promises

--- src/index.js
+++ src/index.js
@@ -1,117 +1,132 @@
 'use strict';
 
 var clientConnector = require( './lib/clientConnector.js' );
+var _ = require( 'lodash' );
 var dateFormat = require( 'dateformat' );
-
-// Suspend logstream functionality for now
-// var logStream = require( './lib/logStream.js' );
 var Q = require( 'q' );
 var NodeCache = require( 'node-cache' );
 var UUID = require( 'uuid-js' );
-
+var levels = [ 'error', 'warn', 'info', 'verbose', 'debug', 'silly' ];
 var refDeferredPairCache = new NodeCache( { stdTTL: 5, checkperiod: 1 } );
-var severities = [ 'error', 'warn', 'info', 'verbose', 'debug', 'silly' ];
-var socket = clientConnector.socket;
-var pscope;
-var message;
+var inbound = clientConnector.inbound;
 
-var Logger = function( opts ) {
-  var store = opts.store;
-  clientConnector.startConnecting( opts.uri );
-  clientConnector.connectToOutbound();
-
-  clientConnector.sendConnectionRequest();
-  clientConnector.startHeartBeat();
-
-  return {
-    level: severities[ 2 ],
-
-    time: function() {
-      return new Date();
-    },
-
-    pscope: 'server',
-    store: store,
-
-    info: function( message ) {
-      this.level = severities[ 2 ];
-      this.log( 'info', message, function() {
-
-      } );
-
-      return this;
-    },
-
-    scope: function( msg ) {
-      this.pscope = msg;
-      return this;
-    },
-
-    log: function( sevLevel, options ) {
-      let deferred = Q.defer();
-
-      if ( !options ) {
-        deferred.reject( 'Invalid arguments' );
-      }
-
-      if ( typeof options !== 'string' ) {
-        this.pscope = options.scope !== undefined ? options.scope : 'server';
-        message = options;
-      } else {
-        pscope = ( options.scope !== undefined ) ? options.scope : 'server';
-        message = options;
-      }
-
-      var sendLog = {
-        ref: UUID.create( 1 ).toString(),
-        operation: 'send',
-        store: this.store,
-        payload: {
-          pdata: message,
-          pscope: this.pscope,
-          level: sevLevel,
-          timestamp: dateFormat( 'mmmm dd HH:MM:ss' )
-        }
-      };
-
-      socket.send( JSON.stringify( sendLog ) );
-      refDeferredPairCache.set( sendLog.ref, deferred );
-      this.pscope = 'server';
-
-      return deferred.promise;
-    },
-
-    socket: socket
-
-    // *** Reconstruct later
-    // realtime: function( data ) {
-    //   logStream( clientConnector.subscriber, store, data );
-    // }
-  };
-};
-
-socket.on( 'message', ( response ) => {
+inbound.on( 'message', ( response ) => {
   let jsonResponse = JSON.parse( response.toString() );
   var deferred = refDeferredPairCache.get( jsonResponse.ref );
   if ( deferred ) {
-    refDeferredPairCache.del( jsonResponse.ref );
     deferred.resolve( jsonResponse );
+    refDeferredPairCache.del( jsonResponse.ref );
   }
 } );
 
+class Logbin {
+  constructor( opts ) {
+    inbound.connect( opts.uri || 'tcp://127.0.0.1:5555' );
+    this.store = opts.store;
+    this.pscope = opts.scope || 'server';
+    this.requestTTL = ( opts.reqTTL / 1000 ) || 5;
+  }
+
+  setStore( store ) {
+    this.store = store;
+    return this;
+  }
+
+  setScope( scope ) {
+    this.scope = scope;
+    return this;
+  }
+
+  error( input ) {
+    return this.log( 'error', input );
+  }
+
+  warn( input ) {
+    return this.log( 'warn', input );
+  }
+
+  info( input ) {
+    return this.log( 'info', input );
+  }
+
+  verbose( input ) {
+    return this.log( 'verbose', input );
+  }
+
+  debug( input ) {
+    return this.log( 'debug', input );
+  }
+
+  silly( input ) {
+    return this.log( 'silly', input );
+  }
+
+  log( level, input ) {
+    var deferred = Q.defer();
+
+    if ( !input ) {
+      let error = {
+        code: 'EMISSINGARG',
+        message: 'Missing log arguments'
+      };
+
+      deferred.reject( error );
+    }
+
+    if ( levels.indexOf( level ) === -1 ) {
+      let error = {
+        code: 'ELEVELINVALID',
+        message: 'Invalid level: ' + level,
+        input: input,
+        validLevels: levels
+      };
+      deferred.reject( error );
+    }
+
+    let partialPayload = {
+      '@pscope': this.scope,
+      '@level': level,
+      '@timestamp': dateFormat( 'mmmm dd HH:MM:ss' )
+    };
+
+    let fullPayload;
+    if ( typeof( input ) === 'string' ) {
+      partialPayload[ '@message' ] = input;
+      fullPayload = partialPayload;
+    } else if ( typeof( input ) === 'object' ) {
+      fullPayload = _.merge( partialPayload, input );
+    }
+
+    let request = {
+      ref: UUID.create( 1 ).toString(),
+      operation: 'send',
+      store: this.store,
+      payload: fullPayload
+    };
+
+    refDeferredPairCache.set( request.ref, deferred );
+    inbound.send( JSON.stringify( request ) );
+
+    return deferred.promise;
+  }
+
+}
+
+// Handle 'on expire' events of node-cache elements
 refDeferredPairCache.on( 'expired', ( ref, deferred ) => {
-  deferred.reject( 'No response received for message with reference id: ' + ref );
+  let error = {
+    code: 'ENORESPONSE',
+    message: 'No response from server.'
+  };
+  deferred.reject( error );
 } );
-
-var disconnect = {
-  payload: {
-    operation: 'disconnect'
-  }
-};
 
 process.on( 'SIGINT', () => {
-  socket.send( JSON.stringify( disconnect ) );
   process.exit();
 } );
 
-module.exports = Logger;
+function instanceOfLogbin ( opts ) {
+  return new Logbin( opts );
+}
+
+module.exports = instanceOfLogbin;
