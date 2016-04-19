6804a34c8f2fda76c7563f975f276f1243dab707
Christopher Clint B. Pacillos <clint.pacillos@gmail.com>
2016-04-19 16:33:35 +0800

    refactor(client-api): realtime log stream api * dev issue: https://identifi.atlassian.net/browse/LS-104 * move RealTime object from old index to log-stream.js

--- src/log-stream.js
+++ src/log-stream.js
@@ -1,1 +1,62 @@
 'use strict';
+
+import assert       from 'assert';
+import zmq          from 'zmq';
+import _            from 'lodash';
+import EventEmitter from 'events';
+import uuid         from 'uuid-js';
+
+export default class LogStream extends EventEmitter {
+  constructor( opts ) {
+    super();
+
+    assert( opts.store, `'store' is not specified` );
+    assert( opts.token, `'token' is not specified` );
+
+    this._opts = _.merge( {
+      uri: 'tcp://127.0.0.1:5556',
+      filter: {
+        level: 'silly'
+      }
+    }, opts || {} );
+
+    this._socket = zmq.socket( 'dealer' );
+
+    // jscs: disable
+    this._socket[ 'plain_password' ] = this._opts.token;
+
+    this._socket.on( 'message', ( data ) => {
+      let jsonData = JSON.parse( data.toString() );
+
+      if ( jsonData.operation === 'SEND_LOG' ) {
+        this.emit( 'log', jsonData.payload );
+      }
+
+    } );
+
+    this._socket.connect( this._opts.uri );
+    this._subscribe();
+  }
+
+  _subscribe() {
+    let request = {
+      ref: uuid.create( 1 ).toString(),
+      operation: 'SUBSCRIBE',
+      store: this._opts.store,
+      filter: this._opts.filter
+    };
+
+    this._socket.send( JSON.stringify( request ) );
+  }
+
+  set filter( filter ) {
+    this._opts.filter = _.merge( {
+      level: 'silly'
+    }, filter || {} );
+    this._subscribe();
+  }
+
+  get filter() {
+    return this._opts.filter;
+  }
+}
