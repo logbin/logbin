4f49636b18d5edb1341051bc8460cf0f701232ed
Christopher Clint B. Pacillos <clint.pacillos@gmail.com>
2016-04-20 18:59:18 +0800

    fix(index.js): use var instead of let in index

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var Logger = require( './dist/logger' );
 Logger.LogStream = require( './dist/log-stream' );
 
@@ -8,10 +6,10 @@ module.exports = Logger;
 // Start dummy authentication layer.
 // Remove this when server is already deployed.
 
-let zmqzap = require( 'zmq-zap' );
-let zmq = require( 'zmq' );
+var zmqzap = require( 'zmq-zap' );
+var zmq = require( 'zmq' );
 
-let ZAP = zmqzap.ZAP,
+var ZAP = zmqzap.ZAP,
   PlainMechanism = zmqzap.PlainMechanism,
   zap = new ZAP();
 
@@ -20,7 +18,7 @@ zap.use( new PlainMechanism( ( data, callback ) => {
   callback( null, true ); // Just grant all connections
 } ) );
 
-let zapSocket = zmq.socket( 'router' );
+var zapSocket = zmq.socket( 'router' );
 zapSocket.on( 'message', function() {
   zap.authenticate( arguments, ( err, response ) => {
     if ( err ) { console.error( 'Error:', err ); }
