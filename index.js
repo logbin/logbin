9c8ed6ec4936df928e369110cfbc83dd277eee00
Christopher Clint B. Pacillos <clint.pacillos@gmail.com>
2016-04-20 19:13:42 +0800

    fix(tests): run a dummy zap layer

--- index.js
+++ index.js
@@ -3,26 +3,7 @@ Logger.LogStream = require( './dist/log-stream' );
 
 module.exports = Logger;
 
-// Start dummy authentication layer.
-// Remove this when server is already deployed.
-
-var zmqzap = require( 'zmq-zap' );
-var zmq = require( 'zmq' );
-
-var ZAP = zmqzap.ZAP,
-  PlainMechanism = zmqzap.PlainMechanism,
-  zap = new ZAP();
-
-// Start authentication layer
-zap.use( new PlainMechanism( ( data, callback ) => {
-  callback( null, true ); // Just grant all connections
-} ) );
-
-var zapSocket = zmq.socket( 'router' );
-zapSocket.on( 'message', function() {
-  zap.authenticate( arguments, ( err, response ) => {
-    if ( err ) { console.error( 'Error:', err ); }
-    if ( response ) { zapSocket.send( response ); }
-  } );
-} );
-zapSocket.bindSync( 'inproc://zeromq.zap.01' );
+// Remove the following 2 lines when the
+// server is already deployed.
+var dummyZapLayer = require( './dist/dummy-auth-layer' );
+dummyZapLayer.init();
